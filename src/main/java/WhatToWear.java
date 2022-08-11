ba10d21da734062c2594c8637a2f2335fcb35ac6
Heying778 <yingyinghe2022@gmail.com>
2022-08-11 16:07:29 +0100

    start debugging

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class WhatToWear {
     public static void main(String[] args) {
-        System.out.println("Please tell me the weather and the temperature, I will guide you what to wear:)");
-        System.out.println("Please input the weather here:");
-        System.out.println("Please input the temperature with only number here:");
+        System.out.println("Please tell me the weather and the temperature, I will guide you what to wear:)"
+        +"Please input your weather:");
+
         Scanner reader = new Scanner(System.in);
         String weather = reader.nextLine();
         int temperature = reader.nextInt();
+        System.out.println("Please input your weather:");
+        System.out.println("Please input your temperature:");
         switch (weather){
             case "Rainy":
                 System.out.println("with your umbrella");
@@ -22,9 +24,11 @@ public class WhatToWear {
             System.out.println("A coat");
         } else if(25 >=temperature && temperature>=10){
             System.out.println("A hoodie");
-        }else (temperature >= 26){
+        }else if (temperature>=26)
+        {
             System.out.println("A T-shirt");
         }
+        System.out.println("The suggest is" + temperature + weather);
 
 
 
