3c6227672919ab4cba7289e624b49097c0e28578
Heying778 <yingyinghe2022@gmail.com>
2022-08-11 16:01:14 +0100

    switch & if statements

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -8,7 +8,26 @@ public class WhatToWear {
         Scanner reader = new Scanner(System.in);
         String weather = reader.nextLine();
         int temperature = reader.nextInt();
-        
+        switch (weather){
+            case "Rainy":
+                System.out.println("with your umbrella");
+            case "Sunny":
+                System.out.println("enjoying the sunshie");
+            case "Snow":
+                System.out.println("with your scarf");
+            case "Cloudy":
+                System.out.println("not with your hat");
+        }
+        if(temperature < 10){
+            System.out.println("A coat");
+        } else if(25 >=temperature && temperature>=10){
+            System.out.println("A hoodie");
+        }else (temperature >= 26){
+            System.out.println("A T-shirt");
+        }
+
+
+
 
     }
 }
