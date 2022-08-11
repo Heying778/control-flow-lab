99ec55db5185a31586951ea3f67e05342ec28d11
Heying778 <yingyinghe2022@gmail.com>
2022-08-11 16:39:36 +0100

    Got help from Iain but not all sorted

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -2,24 +2,27 @@ import java.util.Scanner;
 
 public class WhatToWear {
     public static void main(String[] args) {
-        System.out.println("Please tell me the weather and the temperature, I will guide you what to wear:)"
-        +"Please input your weather:");
-
+        System.out.println("Please tell me the weather and the temperature, I will guide you what to wear:)");
+        System.out.println("Please input your weather:");
         Scanner reader = new Scanner(System.in);
         String weather = reader.nextLine();
-        int temperature = reader.nextInt();
-        System.out.println("Please input your weather:");
-        System.out.println("Please input your temperature:");
         switch (weather){
             case "Rainy":
                 System.out.println("with your umbrella");
+                break;
             case "Sunny":
-                System.out.println("enjoying the sunshie");
+                System.out.println("enjoying the sunshine");
+                break;
             case "Snow":
                 System.out.println("with your scarf");
+                break;
             case "Cloudy":
                 System.out.println("not with your hat");
+                break;
         }
+
+        System.out.println("Please input your temperature:");
+        int temperature = reader.nextInt();
         if(temperature < 10){
             System.out.println("A coat");
         } else if(25 >=temperature && temperature>=10){
@@ -29,6 +32,7 @@ public class WhatToWear {
             System.out.println("A T-shirt");
         }
         System.out.println("The suggest is" + temperature + weather);
+
 
 
 
