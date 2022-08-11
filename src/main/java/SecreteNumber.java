e8d44c85d0417adf10701f306a87c7f11c8d16e6
Heying778 <yingyinghe2022@gmail.com>
2022-08-11 15:16:37 +0100

    set up folder structure

---
+++ src/main/java/SecreteNumber.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SecreteNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Guess what is my secrete number?");
+        Scanner reader = new Scanner(System.in);
+    }
+}
