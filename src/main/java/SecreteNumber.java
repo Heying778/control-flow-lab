bce18fdb26d8f5ab45521ec04cba1ccef27fb700
Heying778 <yingyinghe2022@gmail.com>
2022-08-11 15:31:07 +0100

    basic constructions of W1D4 Lab

--- src/main/java/SecreteNumber.java
+++ src/main/java/SecreteNumber.java
@@ -4,6 +4,14 @@ public class SecreteNumber {
 
     public static void main(String[] args) {
         System.out.println("Guess what is my secrete number?");
+        System.out.println("Please remember only type in *NUMBER*!");
         Scanner reader = new Scanner(System.in);
+        int guess = reader.nextInt();
+        if(guess == 77){
+            System.out.println("OMG you did it!");
+        } else if (guess <50 || guess > 100) {
+            System.out.println("Bad one, you are far from it");
+        }
+
     }
 }
