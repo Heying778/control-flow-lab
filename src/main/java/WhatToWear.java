eb44020813b9331fc11844c356a95b470fb4108e
Heying778 <yingyinghe2022@gmail.com>
2022-08-11 15:47:22 +0100

    basic defs of WhatToWear

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class WhatToWear {
     public static void main(String[] args) {
-// put code in here
+        System.out.println("Please tell me the weather and the temperature, I will guide you what to wear:)");
+        System.out.println("Please input the weather here:");
+        System.out.println("Please input the temperature with only number here:");
+        Scanner reader = new Scanner(System.in);
+        String weather = reader.nextLine();
+        int temperature = reader.nextInt();
+        
+
     }
 }
