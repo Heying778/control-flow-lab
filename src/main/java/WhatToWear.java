ba01b3b4d5f4c274ce0de2f97382a42c51d856a8
Heying778 <yingyinghe2022@gmail.com>
2022-08-11 20:15:09 +0100

    After dinner adjustments

--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -2,40 +2,40 @@ import java.util.Scanner;
 
 public class WhatToWear {
     public static void main(String[] args) {
-        System.out.println("Please tell me the weather and the temperature, I will guide you what to wear:)");
-        System.out.println("Please input your weather:");
-        Scanner reader = new Scanner(System.in);
-        String weather = reader.nextLine();
-        switch (weather){
-            case "Rainy":
-                System.out.println("with your umbrella");
-                break;
-            case "Sunny":
-                System.out.println("enjoying the sunshine");
-                break;
-            case "Snow":
-                System.out.println("with your scarf");
-                break;
-            case "Cloudy":
-                System.out.println("not with your hat");
-                break;
-        }
 
+
+        System.out.println("Please tell the temperature and the weather, I will guide you what to wear:)");
+        Scanner reader = new Scanner(System.in);
         System.out.println("Please input your temperature:");
         int temperature = reader.nextInt();
-        if(temperature < 10){
-            System.out.println("A coat");
-        } else if(25 >=temperature && temperature>=10){
-            System.out.println("A hoodie");
-        }else if (temperature>=26)
-        {
-            System.out.println("A T-shirt");
+        System.out.println("Please input your weather:");
+        String weather = reader.nextLine();
+        String tempSuggestion = "";
+        String weatherSuggestion = "";
+
+        if (temperature< 10){
+            tempSuggestion = " A coat";
+        }else if (temperature >=10 && temperature < 25){
+            tempSuggestion = "A hoodie";
+        }else if (temperature >=25){
+            tempSuggestion = "A T-shirt";
         }
-        System.out.println("The suggest is" + temperature + weather);
 
+        switch (weather) {
+            case "Rainy":
+                weatherSuggestion = "with your umbrella";
+                break;
+            case "Sunny":
+                weatherSuggestion = "enjoying the sunshine";
+                break;
+            case "Snow":
+                weatherSuggestion = "with your scarf";
+                break;
+            case "Cloudy":
+                weatherSuggestion = "not with your hat";
+                break;
+        }
 
-
-
-
+        System.out.println("Your suggest is" + tempSuggestion + weather);
     }
 }
